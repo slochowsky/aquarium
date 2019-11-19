54bab9266d8411d79682b9c02c04592147afb07d
blake slochowsky <blakeslochowsky.com>
2019-11-19 11:23:54 -0600

    commit

---
+++ aquarium/main.js
@@ -0,0 +1,10 @@
+/**
+ *  Import the initializeDetailButtonEvents function
+ *  reference and then invoke it
+ */
+import initializeDetailButtonEvents from './dialogs.js'
+import addMouseOverEventListeners from './highlight.js'
+
+
+initializeDetailButtonEvents()
+addMouseOverEventListeners()
